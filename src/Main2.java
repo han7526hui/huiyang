84960d7c4d3bf008141ba280a54ee4b13e3b3875
han7526hui <2933809204@qq.com>
2026-03-26 23:12:35 +0800

    Delete src directory

--- src/Main2.java
+++
@@ -1,7 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        UserRegistration userReg = new UserRegistration();
-        userReg.registration();
-        System.out.println(userReg); 
-    }
-}
